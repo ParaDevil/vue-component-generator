a1f0f8d0d9b02355cf663cddce697fab2d3b2137
DDDrop <wylroro.dev@gmail.com>
2018-04-09 18:15:09 +0900

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Vue Component Generator
+
+This project is a customized version for myself based on [vue-generate-component](https://github.com/NetanelBasal/vue-generate-component).
---
+++ dist/blueprints/component/index.vue
@@ -0,0 +1,3 @@
+<template src="./{{name}}.component.{{ filesType.html }}"></template>
+<script src="./{{name}}.component.{{ filesType.script }}"{% if filesType.script !="js" %} lang="{{ filesType.script }}"{% endif %}></script>
+<style src="./{{name}}.component.{{ filesType.style }}" scoped lang="{{ filesType.style }}"></style>
